72ed8f44feb518227559c3634d765f19ed86f8e0
Gabriela <walczyk.gabriela2@gmail.com>
2021-03-01 18:24:03 +0100

    laravel skeleton with configured adminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
